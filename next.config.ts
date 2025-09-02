e1f592fd2799cb8a426082266efdceca123dfc40
pota2dev <mahdihasan535@gmail.com>
2025-09-03 03:04:40 +0600

    try

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    nodeMiddleware: true, // enable Node.js middleware support (experimental in some Next versions)
-  },
+  // experimental: {
+  //   nodeMiddleware: true, // enable Node.js middleware support (experimental in some Next versions)
+  // },
 };
 
 export default nextConfig;
